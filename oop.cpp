f246cf293ed517ef092f160a03af9f05d681ee7a
Anmol Gupta <146470603+Anmolgupta2004@users.noreply.github.com>
2024-07-11 18:10:49 +0530

    Add files via upload

---
+++ oop.cpp
@@ -0,0 +1,86 @@
+// procedure Orientend Programming 
+// Conventional programming paredigm - use high level language -c,coble ,fortran.
+// program - divided into tasks - groups into functions
+
+//globle data -accessible from entie program 
+// difficult to track which function change data
+// if data stucture is changed, functions to be revised
+
+// OOP (focus not on function only data)
+
+//bind the data to the function using it 
+// program - divided into objects(data+function) 
+// its product data from accedental changes
+
+
+//class  
+
+//the fundamental unit of oop
+//usser defined data types
+// define some data/properties & metods/functions
+
+//object
+
+//variable of type class
+
+
+#include<iostream>
+using namespace std;
+
+class fruit{
+  public:
+  string name;
+  string color;
+} ;
+
+class Student{
+    string name;
+    int roll_no;
+};
+
+int main(){
+
+    fruit apple;//object
+    apple.name="Apple";
+    apple.color="Red";
+
+    cout<<apple.name<<" "<<apple.color<<endl;
+
+    fruit *mango= new fruit();
+    mango->name="Mango";
+    mango->color="Yellow";
+    cout<<mango->name<<" "<<mango->color<<endl;
+
+
+    //default constructor
+
+    Rectangle r1;
+    cout<<r1.l<<" "<<r1.b<<endl;
+    return 0;
+}
+// to run code ctrl+alt+n
+
+// Constructor 
+
+// used to initialise an object 
+// this is function which is called when an object is created
+// same name as class name
+// types
+
+// default
+//parameterised
+//copy
+
+
+class Rectangle{
+    public:
+    int l;
+    int b;
+Rectangle(){// default constructor--no args passed
+    l=0;
+    b=0;
+}
+};
+
+
+
